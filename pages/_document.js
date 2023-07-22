7815d8d9e3d09c9616c496b38fd538d7511ffa7a
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-23 02:01:48 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,22 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
       </body>
+      <script
+        defer
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+      ></script>
     </Html>
   )
 }
