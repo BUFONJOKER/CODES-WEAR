f074a02e3573f6e5649f1dcf3dc42192d6582667
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-24 01:31:15 +0500

    styles css files deleted

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
 import Home from "@/components/Home";
-import Footer from "@/components/Footer";
+
 
 export default function Index() {
   return (
