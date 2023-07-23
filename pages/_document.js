9cafb023411b08d6302b3443d46d193fda7b3a4d
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-23 22:05:19 +0500

    _document.js updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-        />
-      </Head>
-      
+      <Head />
+
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+
       <body>
         <Main />
         <NextScript />
@@ -20,5 +19,5 @@ export default function Document() {
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
       ></script>
     </Html>
-  )
+  );
 }
