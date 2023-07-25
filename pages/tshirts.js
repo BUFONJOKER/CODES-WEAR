c34abd448e4099672bda15439a9f29cdbb5f672f
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-25 23:37:38 +0500

    card added for t shirts

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,7 +1,337 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Tshirts() {
   return (
-    <div>tshirts</div>
+    <>
+
+<section className='m-5'>
+<div className="container m-5">
+<div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+
+   <div className="container m-5">
+   <div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+
+   <div className="container m-5">
+   <div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+
+   <div className="container m-5">
+   <div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+
+   <div className="container m-5">
+   <div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+
+   <div className="container m-5">
+   <div className="row">
+      <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="col-sm-6">
+        <div className="card" style={{ width: "18rem" }}>
+          <Image src="/logo.jpg" width={100} height={100} className="card-img-top" alt=""/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+</section>
+    </>
+
+
+
+  
+
+    // <div>
+    //   <section>
+    //     <div className="container py-5">
+    //       <div className="row">
+    //         <div className="col-md-8 col-xl-6 text-center mx-auto">
+    //           <p className="fw-bold text-success mb-2">Our Services</p>
+    //           <h3 className="fw-bold">What we can do for you</h3>
+    //         </div>
+    //       </div>
+    //       <div className="py-5 p-lg-5">
+    //         <div
+    //           className="row row-cols-1 row-cols-md-2 mx-auto"
+    //           style={{ maxWidth: "900px" }}
+    //         >
+    //           <div className="col mb-5">
+    //             <div className="card shadow-sm">
+    //               <div className="card-body px-4 py-5 px-md-5">
+    //                 <div
+    //                   className="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon"
+    //                   style={{
+    //                     top: "1rem",
+    //                     right: "1rem",
+    //                     position: "absolute",
+    //                   }}
+    //                 >
+                    
+    //                 </div>
+    //                 <h5 className="fw-bold card-title">
+    //                   Lorem ipsum dolor sit&nbsp;
+    //                 </h5>
+    //                 <Image alt = "" src="/logo.jpg" width={50} height={50}/>
+                    
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="col mb-5">
+    //             <div className="card shadow-sm">
+    //               <div className="card-body px-4 py-5 px-md-5">
+    //                 <div
+    //                   className="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon"
+    //                   style={{
+    //                     top: "1rem",
+    //                     right: "1rem",
+    //                     position: "absolute",
+    //                   }}
+    //                 >
+                     
+    //                 </div>
+    //                 <h5 className="fw-bold card-title">
+    //                   Lorem ipsum dolor sit&nbsp;
+    //                 </h5>
+    //                 <p className="text-muted card-text mb-4">
+    //                   Erat netus est hendrerit, nullam et quis ad cras porttitor
+    //                   iaculis. Bibendum vulputate cras aenean.
+    //                 </p>
+    //                 <button className="btn btn-primary shadow" type="button">
+    //                   Learn more
+    //                 </button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="col mb-4">
+    //             <div className="card shadow-sm">
+    //               <div className="card-body px-4 py-5 px-md-5">
+    //                 <div
+    //                   className="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon"
+    //                   style={{
+    //                     top: "1rem",
+    //                     right: "1rem",
+    //                     position: "absolute",
+    //                   }}
+    //                 >
+                    
+    //                 </div>
+    //                 <h5 className="fw-bold card-title">
+    //                   Lorem ipsum dolor sit&nbsp;
+    //                 </h5>
+    //                 <p className="text-muted card-text mb-4">
+    //                   Erat netus est hendrerit, nullam et quis ad cras porttitor
+    //                   iaculis. Bibendum vulputate cras aenean.
+    //                 </p>
+    //                 <button className="btn btn-primary shadow" type="button">
+    //                   Learn more
+    //                 </button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="col mb-4">
+    //             <div className="card shadow-sm">
+    //               <div className="card-body px-4 py-5 px-md-5">
+    //                 <div
+    //                   className="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon"
+    //                   style={{
+    //                     top: "1rem",
+    //                     right: "1rem",
+    //                     position: "absolute",
+    //                   }}
+    //                 >
+                      
+    //                 </div>
+    //                 <h5 className="fw-bold card-title">
+    //                   Lorem ipsum dolor sit&nbsp;
+    //                 </h5>
+    //                 <p className="text-muted card-text mb-4">
+    //                   Erat netus est hendrerit, nullam et quis ad cras porttitor
+    //                   iaculis. Bibendum vulputate cras aenean.
+    //                 </p>
+    //                 <button className="btn btn-primary shadow" type="button">
+    //                   Learn more
+    //                 </button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </section>
+    // </div>
   )
 }
