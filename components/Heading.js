aca99c551e721954aff5f65b5748d3227376faff
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-26 23:56:44 +0500

    head component with title and meta tag

--- components/Heading.js
+++ components/Heading.js
@@ -3,6 +3,12 @@ import Head from 'next/head'
 
 export default function Heading(props) {
   return (
-    <div></div>
+    <div><Head>
+      <title>Codes Wear</title>
+      <meta name="description" content="CodesWear - Wear the code" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/logo.jpg" />
+
+    </Head></div>
   )
 }
