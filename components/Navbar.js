3116f09d563918c77c5bcdd710640fb068714d00
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-27 02:21:16 +0500

    cart card updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import React, { useRef } from "react";
 import Image from "next/image";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-
+import { AiOutlineShoppingCart, AiOutlinePlusCircle,
+   AiOutlineMinusCircle, AiFillCloseCircle } from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
 export default function Navbar() {
 
   //ref for cart
@@ -30,7 +31,10 @@ export default function Navbar() {
       >
         <div className="container">
           <Link className="navbar-brand  d-flex align-items-center" href="/">
-            <span className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon">
+            <span 
+            className="bs-icon-sm 
+            bs-icon-circle bs-icon-primary shadow
+             d-flex justify-content-center align-items-center me-2 bs-icon">
               <Image src="/logo.jpg" alt="" width={60} height={60}></Image>
             </span>
             <span className="fs-2">Codes Wear</span>
@@ -90,18 +94,64 @@ export default function Navbar() {
               <li className="nav-item">
 
 
-                <Link onClick={cartClick} className="m-2 nav-link text-white" data-bs-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <Link onClick={cartClick} 
+                className="m-2 nav-link text-white" 
+                data-bs-toggle="collapse" 
+                href="#collapseExample" 
+                aria-expanded="false" 
+                aria-controls="collapseExample">
                   <AiOutlineShoppingCart className="fs-1" />
                 </Link>
 
 
               </li>
-              <li className="nav-item">
+              <li className="nav-item fixed-top">
 
                 <div ref={ref} className="collapse" id="collapseExample">
-                  <div className="card card-body" style={{ width: "31%", height: "200px", position: "absolute", right: 6 }}>
-                    <button type="button" className="btn-close m-3" aria-label="Close" onClick={closeClick} style={{ position: "absolute", right: 0 }}></button>
+
+                  <div className="card card-body " style={{ width: "300px", 
+                  height: "300px", position: "absolute", 
+                  right: 6, backgroundColor: "gray" }}>
+                    <h2 className="text-white">Shopping Cart</h2>
+                    <ul>
+
+                      <li className="fs-4 text-white">T-Shirts
+                        &nbsp;&nbsp;&nbsp;
+                        <AiOutlineMinusCircle></AiOutlineMinusCircle>1
+                        <AiOutlinePlusCircle></AiOutlinePlusCircle>
+
+                      </li>
+
+                      <li className="fs-4 text-white">Hoodies
+                        &nbsp;&nbsp;&nbsp;
+                        <AiOutlineMinusCircle></AiOutlineMinusCircle>1
+                        <AiOutlinePlusCircle></AiOutlinePlusCircle>
+                      </li>
+
+                      <li className="fs-4 text-white">Mugs
+                        &nbsp;&nbsp;&nbsp;
+                        <AiOutlineMinusCircle></AiOutlineMinusCircle>1
+                        <AiOutlinePlusCircle></AiOutlinePlusCircle>
+                      </li>
+
+                      <li className="fs-4 text-white">Stickers
+                        &nbsp;&nbsp;&nbsp;
+                        <AiOutlineMinusCircle></AiOutlineMinusCircle>1
+                        <AiOutlinePlusCircle></AiOutlinePlusCircle>
+                      </li>
+                    </ul>
+                    <button type="button"
+                     className="btn btn-primary m-3 rounded-pill fs-2">
+                      <BsFillBagCheckFill className="mb-3 mx-2">
+                        </BsFillBagCheckFill>Checkout</button>
+                    <AiFillCloseCircle className="btn btn-hover" 
+                    onClick={closeClick} 
+                    style={{ position: "absolute", right: 0, width: '70px',
+                     height: '70px' }} />
+
+
                   </div>
+
                 </div>
 
 
