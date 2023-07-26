fbe57515134bedd26700b248fe5298f46bd71d89
ABDUL REHMAN JAVAID <S21BDOCS1M01013@iub.edu.pk>
2023-07-27 01:30:11 +0500

    cart added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,89 +1,127 @@
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function Navbar() {
+
+  //ref for cart
+  const ref = useRef(null);
+
+  //cart click function
+  const cartClick = () => {
+
+
+  }
+
+  //close card if clicked close button
+  const closeClick = () => {
+    ref.current.classList.remove("show");
+
+  }
+
+
   return (
-    <nav
-      className="navbar shadow-lg navbar-expand-md sticky-top py-3 navbar-dark"
-      style={{ backgroundColor: "black" }}
-      id="mainNav"
-    >
-      <div className="container">
-        <Link className="navbar-brand  d-flex align-items-center" href="/">
-          <span className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon">
-            <Image src="/logo.jpg" alt="" width={60} height={60}></Image>
-          </span>
-          <span className="fs-2">Codes Wear</span>
-        </Link>
-        <button
-          data-bs-toggle="collapse"
-          className="navbar-toggler"
-          data-bs-target="#navcol-1"
-        >
-          <span className="visually-hidden">Toggle navigation</span>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navcol-1">
-          <ul className="navbar-nav m-auto">
-            <li className="nav-item">
-              <Link className="nav-link active fs-4" href="/">
-                Home
-              </Link>
-            </li>
+    <>
+      <nav
+        className="navbar shadow-lg navbar-expand-md sticky-top py-3 navbar-dark"
+        style={{ backgroundColor: "black" }}
+        id="mainNav"
+      >
+        <div className="container">
+          <Link className="navbar-brand  d-flex align-items-center" href="/">
+            <span className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon">
+              <Image src="/logo.jpg" alt="" width={60} height={60}></Image>
+            </span>
+            <span className="fs-2">Codes Wear</span>
+          </Link>
+          <button
+            data-bs-toggle="collapse"
+            className="navbar-toggler"
+            data-bs-target="#navcol-1"
+          >
+            <span className="visually-hidden">Toggle navigation</span>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navcol-1">
+            <ul className="navbar-nav mt-4">
+              <li className="nav-item">
+                <Link className="nav-link active fs-4" href="/">
+                  Home
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link className="nav-link fs-4" href="about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link fs-4" href="contact">
-                Contacts
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="dropdown-toggle nav-link fs-4 "
-                aria-expanded="false"
-                data-bs-toggle="dropdown"
-                href="/"
-              >
-                Products{" "}
-              </Link>
-              <div className="dropdown-menu">
-                <Link className="dropdown-item" href="tshirts">
-                  T-Shirts
+              <li className="nav-item">
+                <Link className="nav-link fs-4" href="about">
+                  About
                 </Link>
-                <Link className="dropdown-item" href="hoodies">
-                  Hoodies
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fs-4" href="contact">
+                  Contacts
                 </Link>
-                <Link className="dropdown-item" href="mugs">
-                  Mugs
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="dropdown-toggle nav-link fs-4 "
+                  aria-expanded="false"
+                  data-bs-toggle="dropdown"
+                  href="/"
+                >
+                  Products{" "}
                 </Link>
-                <Link className="dropdown-item" href="stickers">
-                  Stickers
-                </Link>
-              </div>
-            </li>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" href="tshirts">
+                    T-Shirts
+                  </Link>
+                  <Link className="dropdown-item" href="hoodies">
+                    Hoodies
+                  </Link>
+                  <Link className="dropdown-item" href="mugs">
+                    Mugs
+                  </Link>
+                  <Link className="dropdown-item" href="stickers">
+                    Stickers
+                  </Link>
+                </div>
+              </li>
 
-            
-          <li className="nav-item">
-            <Link className="nav-link text-white" href="/">
-              <AiOutlineShoppingCart className="fs-1" />
-            </Link>
-          </li>
-          <li className="nav-item mx-3">
-            <Link className="btn btn-primary shadow fs-4 rounded-pill" role="button" href="/">
-              Sign up
-            </Link>
-          </li>
-          </ul>
 
-          
+              <li className="nav-item">
+
+
+                <Link onClick={cartClick} className="m-2 nav-link text-white" data-bs-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                  <AiOutlineShoppingCart className="fs-1" />
+                </Link>
+
+
+              </li>
+              <li className="nav-item">
+
+                <div ref={ref} className="collapse" id="collapseExample">
+                  <div className="card card-body" style={{ width: "31%", height: "200px", position: "absolute", right: 6 }}>
+                    <button type="button" className="btn-close m-3" aria-label="Close" onClick={closeClick} style={{ position: "absolute", right: 0 }}></button>
+                  </div>
+                </div>
+
+
+              </li>
+
+
+
+            </ul>
+
+
+          </div>
+
+
+
         </div>
-      </div>
-    </nav>
+      </nav>
+
+
+
+
+    </>
   );
 }
