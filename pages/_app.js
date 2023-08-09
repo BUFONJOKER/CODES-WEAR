bd375ad045908ff564cc134c86823453112d1670
BUFONJOKER <s21bdocs1m01013@iub.edu.pk>
2023-08-09 18:39:28 +0500

    cart,total state, add to cart function

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,20 @@ import Heading from "@/components/Heading";
 
 
 export default function App({ Component, pageProps }) {
+
+  // cart state
+  const [cart, setCart] = useState({});
+
+  // cart total state
+  const [subTotal, setSubTotal] = useState(0);
+
+  const addToCart = (itemCode,quantity,price,name,variant,size)=>{
+    
+    let myCart = cart;
+
+    
+  }
+
   return (
     <>
       <Heading />
