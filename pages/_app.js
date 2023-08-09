e780619eeb48887393ea8484d48f7d8ddc639f1a
BUFONJOKER <s21bdocs1m01013@iub.edu.pk>
2023-08-09 19:35:35 +0500

    add,save,clear,remove cart functions made

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Heading from "@/components/Heading";
+import { useState, useEffect } from "react";
 
 
 export default function App({ Component, pageProps }) {
@@ -11,11 +12,88 @@ export default function App({ Component, pageProps }) {
   // cart total state
   const [subTotal, setSubTotal] = useState(0);
 
-  const addToCart = (itemCode,quantity,price,name,variant,size)=>{
-    
-    let myCart = cart;
+  // use effect to check if cart is in local storage
+  useEffect(() => {
+    console.log('useEffect in _app.js');
 
-    
+    try {
+
+      if (localStorage.getItem('cart')) {
+        setCart(JSON.parse(localStorage.getItem('cart')));
+      }
+
+    }
+
+    catch (error) {
+      console.log(error);
+    }
+  }, [])
+
+
+
+  // add to cart function
+  const addToCart = (itemCode, quantity, price, name, variant, size) => {
+
+    // create a copy of the cart state
+    let newCart = cart;
+
+    // check if item is already in cart
+    if (itemCode in cart) {
+      // if item is in cart, increase quantity
+      newCart[itemCode].quantity = cart[itemCode].quantity + quantity;
+    }
+
+    // if item is not in cart, add it
+    else {
+      // add item to cart
+      newCart[itemCode] = {
+        quantity: 1,
+        price: price,
+        name: name,
+        variant: variant,
+        size: size
+      }
+    }
+
+    // update cart state
+    setCart(newCart);
+
+    // update cart in local storage
+    sveCart(JSON.stringify(newCart));
+  }
+
+  // update cart in local storage
+  const saveCart = (newCart) => {
+
+    localStorage.setItem('cart', newCart);
+  }
+
+  // clear cart function
+  const clearCart = () => {
+
+    // clear cart state
+    setCart({});
+
+    // clear cart in local storage
+    saveCart({});
+  }
+
+  // remove item from cart function
+  const removeFromCart = (itemCode) => {
+
+    // create a copy of the cart state
+    let newCart = cart;
+
+    // check if item is in cart then decrease quantity 
+    if (itemCode in cart) {
+      newCart[itemCode].quantity = cart[itemCode].quantity - 1;
+    }
+
+    // if item quantity is 0, remove it from cart
+    if (newCart[itemCode].quantity <= 0) {
+      delete newCart[itemCode];
+    }
+
   }
 
   return (
