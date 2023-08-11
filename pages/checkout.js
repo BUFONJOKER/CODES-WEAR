5fbec515341442fd7d22c2f6872e00b61f463e02
BUFONJOKER <s21bdocs1m01013@iub.edu.pk>
2023-08-12 00:07:59 +0500

    checkout page created

--- pages/checkout.js
+++ pages/checkout.js
@@ -2,6 +2,66 @@ import React from 'react'
 
 export default function Checkout() {
   return (
-    <div>checkout</div>
+    <div className="container text-white mt-5 mb-5">
+      <h1 className="text-center">Checkout</h1>
+      <label htmlhtmlFor="name">Name</label>
+      <div className="row">
+        <div className="col">
+          <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+        </div>
+        <div className="col">
+          <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+        </div>
+      </div>
+
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label htmlFor="inputEmail4" className="form-label">Email</label>
+          <input type="email" className="form-control" id="inputEmail4" />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputPassword4" className="form-label">Password</label>
+          <input type="password" className="form-control" id="inputPassword4" />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress" className="form-label">Address</label>
+          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputCity" className="form-label">City</label>
+          <input type="text" className="form-control" id="inputCity" />
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="inputState" className="form-label">State</label>
+          <select id="inputState" className="form-select">
+            <option selected>Choose...</option>
+            <option>Punjab</option>
+            <option>Sindh</option>
+            <option>Baluchistan</option>
+            <option>KPK</option>
+          </select>
+        </div>
+        <div className="col-md-2">
+          <label htmlFor="inputZip" className="form-label">Zip</label>
+          <input type="text" className="form-control" id="inputZip" />
+        </div>
+        <div className="col-12">
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" id="gridCheck" />
+            <label className="form-check-label" htmlFor="gridCheck">
+              Check me out
+            </label>
+          </div>
+        </div>
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">Continue to Shipping</button>
+        </div>
+      </form>
+
+    </div>
   )
 }
