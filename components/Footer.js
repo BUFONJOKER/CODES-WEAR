4aab6939e0ab42e593aa0a5a4adeb08d42d6e6bc
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-16 01:15:28 +0500

    signUp page created

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div>
-      <footer className="bg-dark">
+      <footer className="bg-dark mt-5">
         <div className="container py-4 py-lg-5">
           <div className="row justify-content-center">
             <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
--- pages/login.js
+++ pages/login.js
@@ -1,56 +1,57 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Login() {
   return (
     <div>
-    <section class="h-100 gradient-form" style={{backgroundColor: "#121F3E"}}>
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-xl-10">
-        <div class="card rounded-3 text-black">
-          <div class="row g-0">
-            <div class="col-lg-6">
-              <div class="card-body p-md-5 mx-md-4">
+    <section className="h-100 gradient-form" style={{backgroundColor: "#121F3E"}}>
+  <div className="container py-5 h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-xl-10">
+        <div className="card rounded-3 text-black">
+          <div className="row g-0">
+            <div className="col-lg-6">
+              <div className="card-body p-md-5 mx-md-4">
 
-                <div class="text-center">
+                <div className="text-center">
                   <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
                     style={{ width: '185px' }} alt="logo"/>
-                  <h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+                  <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
                 </div>
 
                 <form>
                   <p>Please login to your account</p>
 
-                  <div class="form-outline mb-4">
-                    <input type="email" id="form2Example11" class="form-control"
+                  <div className="form-outline mb-4">
+                    <input type="email" id="form2Example11" className="form-control"
                       placeholder="Phone number or email address" />
-                    <label class="form-label" for="form2Example11">Username</label>
+                    <label className="form-label" for="form2Example11">Username</label>
                   </div>
 
-                  <div class="form-outline mb-4">
-                    <input type="password" id="form2Example22" class="form-control" />
-                    <label class="form-label" for="form2Example22">Password</label>
+                  <div className="form-outline mb-4">
+                    <input type="password" id="form2Example22" className="form-control" />
+                    <label className="form-label" for="form2Example22">Password</label>
                   </div>
 
-                  <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                  <div className="text-center pt-1 mb-5 pb-1">
+                    <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
                       in</button>
-                    <a class="text-muted" href="#!">Forgot password?</a>
+                    <a className="text-muted" href="#!">Forgot password?</a>
                   </div>
 
-                  <div class="d-flex align-items-center justify-content-center pb-4">
-                    <p class="mb-0 me-2">Do not have an account?</p>
-                    <button type="button" class="btn btn-outline-danger">Create new</button>
+                  <div className="d-flex align-items-center justify-content-center pb-4">
+                    <p className="mb-0 me-2">Do not have an account?</p>
+                    <Link href= "signup" className="btn btn-outline-danger">Create new</Link>
                   </div>
 
                 </form>
 
               </div>
             </div>
-            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">We are more than just a company</h4>
-                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                <h4 className="mb-4">We are more than just a company</h4>
+                <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               </div>
