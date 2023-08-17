6a0ad62ef6d794ff7b05de6f2ea4eb6d4d962765
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-17 21:07:38 +0500

    mongoose connection

---
+++ middleware/mongoose.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default async function connectDatabase(handler) {
+    if(mongoose.connections[0].readyState){
+
+        return handler(req,res);
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
+    return handler(req,res);
+}
