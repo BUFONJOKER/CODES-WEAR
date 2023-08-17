fe8c951fd77ebee0ef475ff6cb4e11eb6c46a4af
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-17 22:43:15 +0500

    formt

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -2,10 +2,10 @@ import Product from "@/models/Product";
 import connectDb from "@/middleware/mongoose";
 
 const handler = async (req, res) => {
-   
+
         let products = await Product.find();
         console.log(products)
-        res.status(200).json({products});
+        res.status(200).json({ products });
 
 };
 
