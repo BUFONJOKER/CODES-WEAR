ad923a865403b1ce86cea4e2c394162e68f32405
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-17 20:47:16 +0500

    product schema created

---
+++ models/Product.js
@@ -0,0 +1,28 @@
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+// Product schema for mongodb
+const ProductSchema = new mongoose.Schema({
+    
+    title:{type:String,required:true},
+
+    slug:{type:String,required:true,unique:true},
+
+    category:{type:String,required:true},
+
+    size:{type:String},
+
+    color:{type:String,required:true},
+
+    image:{type:String,required:true},
+
+    description:{type:String,required:true},
+    
+    price:{type:Number,required:true},
+
+    availableQuantity:{type:Number,required:true},
+
+}, { timestamps: true });
+
+// export Product model
+export default mongoose.model("Product", ProductSchema);
