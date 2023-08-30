392e6610f404874da6980ada02dba0c851b52b32
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-08-30 23:23:19 +0500

    formatting

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -3,39 +3,23 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 
-
 export default function Tshirts({ products }) {
-
   return (
     <>
       <Head>
         <title>Codes Wear-T-Shirts</title>
       </Head>
-      <h1 className="text-white   fw-bolder fst-italic text-center m-4 fs-1">
+      <h1 className="text-white fw-bolder fst-italic text-center m-4 fs-1">
         T-Shirts for Boys
       </h1>
-
-
 
       {products.map((item) => {
         return (
           <React.Fragment key={item._id}>
-            <div className="container d-flex justify-content-center align-items-center" >
-
-              <div
-                className="card w-50 m-3 text-center"
-
-              >
-                <Link
-                  legacyBehavior
-                  href={`/${item.title}`}
-                  passHref
-                >
-                  <a
-                    rel="preload"
-                    as="image"
-                    href={`${item.image}`}
-                  >
+            <div className="container d-flex justify-content-center align-items-center">
+              <div className="card w-50 m-3 text-center">
+                <Link legacyBehavior href={`/${item.title}`} passHref>
+                  <a rel="preload" as="image" href={`${item.image}`}>
                     <Image
                       src={`${item.image}`}
                       width={100}
@@ -54,39 +38,24 @@ export default function Tshirts({ products }) {
                   <h5 className="card-title">{item.title}</h5>
                 </div>
                 <ul className="list-group list-group-flush">
-                  <li
-                    className="list-group-item"
-                    key={`${item._id}-price`}
-                  >
+                  <li className="list-group-item" key={`${item._id}-price`}>
                     <b>Price:</b> {item.price}
                   </li>
-                  <li
-                    className="list-group-item"
-                    key={`${item._id}-color`}
-                  >
+                  <li className="list-group-item" key={`${item._id}-color`}>
                     <b>Color:</b> {item.color}
                   </li>
-                  <li
-                    className="list-group-item"
-                    key={`${item._id}-size`}
-                  >
+                  <li className="list-group-item" key={`${item._id}-size`}>
                     <b>Size:</b> {item.size}
                   </li>
                 </ul>
               </div>
-
-
             </div>
           </React.Fragment>
         );
       })}
-
-
     </>
-
   );
 }
-
 
 export async function getServerSideProps(context) {
   const res = await fetch("http://localhost:3000/api/getproducts");
