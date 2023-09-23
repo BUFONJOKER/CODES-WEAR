82c51626be0f24595ea7fe1bcdc596d767d337ac
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-09-24 01:30:57 +0500

    formatted

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,28 +1,26 @@
 import User from "@/models/User";
 import connectDB from "@/middleware/mongoose";
 let CryptoJS = require("crypto-js");
+
 const handler = async (req, res) => {
     if (req.method == 'POST') {
-        let user = await User.findOne({ email: req.body.email});
-        
-        if(user){
-            let bytes  = CryptoJS.AES.decrypt(user.password, 'helloworld');
+        let user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            let bytes = CryptoJS.AES.decrypt(user.password, 'helloworld');
             let originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
             if (user.email == req.body.email && originalPassword == req.body.password) {
-                res.status(200).json({ success:true,
-             name: user.name, email: user.email, password: originalPassword });
+                res.status(200).json({
+                    success: true,
+                    name: user.name, email: user.email, password: originalPassword
+                });
             }
         }
-        else{
-            res.status(200).json({ success:false,message:"Invalid Credentials" });
+        else {
+            res.status(200).json({ success: false, message: "Invalid Credentials" });
         }
-        
-        
-       
     }
-
-    
 }
 
 export default connectDB(handler);
