f633ac33729fb8a5f27a94806c024e3f6fe14f34
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-09-24 00:43:10 +0500

    updated

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -5,11 +5,11 @@ const handler = async (req, res) => {
     if (req.method == 'POST') {
         let u = new User(req.body);
         await u.save();
-        res.status(200).json({ message: "User added successfully" });
+        res.status(200).json({ message: "User Created successfully" });
     }
 
     else {
-        res.status(400).json({ message: "User not added" });
+        res.status(400).json({ message: "User not created" });
     }
 }
 
