33ba9e3448bcf827f550b7f1ae2eea84cad3905a
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-09-24 01:03:26 +0500

    password encrypted using crypto-js library

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,10 +1,15 @@
 import User from "@/models/User";
 import connectDB from "@/middleware/mongoose";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
-        let u = new User(req.body);
+      
+        let u = new User({name:req.body.name,
+                        email:req.body.email,
+                        password:CryptoJS.AES.encrypt(req.body.password, 'helloworld').toString()});
         await u.save();
+        console.log(u);
         res.status(200).json({ message: "User Created successfully" });
     }
 
