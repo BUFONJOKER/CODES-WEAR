77b1713be8f7302e02517894857941575fbeb298
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-09-24 00:41:39 +0500

    formatted

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ import { useRouter } from 'next/router'
 export default function Login() {
 
   const router = useRouter()
-  
+
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
@@ -43,7 +43,7 @@ export default function Login() {
     let response = await res.json()
 
     console.log(response)
-    if(response.success){
+    if (response.success) {
       toast.success('🦄 Logged In Successfully', {
         position: "top-center",
         autoClose: 1000,
@@ -54,7 +54,7 @@ export default function Login() {
         progress: undefined,
         theme: "colored",
       })
-  
+
       setEmail("")
       setPassword("")
 
@@ -62,10 +62,10 @@ export default function Login() {
         router.push('http://localhost:3000')
       }, 1100);
 
-      
+
     }
 
-    else{
+    else {
       toast.error('🦄 Invalid Credentials', {
         position: "top-center",
         autoClose: 500,
@@ -76,99 +76,99 @@ export default function Login() {
         progress: undefined,
         theme: "colored",
       })
-  
+
       setEmail("")
       setPassword("")
     }
 
 
-    
+
   }
 
-  
-return (
-  <>
 
-    <Head>
-      <title>Codes Wear-Log In</title>
-    </Head>
-    <h1 className="text-white fw-bolder fst-italic text-center m-4 fs-1">
-      Log In for Codes Wear Account
-    </h1>
-    <ToastContainer
-      position="top-center"
-      autoClose={1000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-    />
-    <div>
-      <section className=" mt-5 mb-5" style={{ backgroundColor: "#121F3E" }}>
-        <div className="container h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-12 col-xl-11">
-              <div className="card text-black" style={{ borderRadius: "25px" }}>
-                <div className="card-body p-md-5">
-                  <div className="row justify-content-center">
-                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+  return (
+    <>
 
-                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log In</p>
+      <Head>
+        <title>Codes Wear-Log In</title>
+      </Head>
+      <h1 className="text-white fw-bolder fst-italic text-center m-4 fs-1">
+        Log In for Codes Wear Account
+      </h1>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <div>
+        <section className=" mt-5 mb-5" style={{ backgroundColor: "#121F3E" }}>
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-lg-12 col-xl-11">
+                <div className="card text-black" style={{ borderRadius: "25px" }}>
+                  <div className="card-body p-md-5">
+                    <div className="row justify-content-center">
+                      <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                      <form method='POST' className="mx-1 mx-md-4">
+                        <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log In</p>
+
+                        <form method='POST' className="mx-1 mx-md-4">
 
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <label className="form-label" htmlFor="email">Email</label>
-                            <input onChange={handleFormChange} value={email} type="email" id="email" name='email' className="form-control" />
+                          <div className="d-flex flex-row align-items-center mb-4">
+                            <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                            <div className="form-outline flex-fill mb-0">
+                              <label className="form-label" htmlFor="email">Email</label>
+                              <input onChange={handleFormChange} value={email} type="email" id="email" name='email' className="form-control" />
 
+                            </div>
                           </div>
-                        </div>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <label className="form-label" htmlFor="password">Password</label>
-                            <input onChange={handleFormChange} value={password} type="password" id="password" name='password' className="form-control" />
+                          <div className="d-flex flex-row align-items-center mb-4">
+                            <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                            <div className="form-outline flex-fill mb-0">
+                              <label className="form-label" htmlFor="password">Password</label>
+                              <input onChange={handleFormChange} value={password} type="password" id="password" name='password' className="form-control" />
 
+                            </div>
                           </div>
-                        </div>
 
 
 
-                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button onClick={handleFormSubmit} type="button" className="btn btn-primary btn-lg">Log In</button>
-                        </div>
+                          <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                            <button onClick={handleFormSubmit} type="button" className="btn btn-primary btn-lg">Log In</button>
+                          </div>
 
-                        <div className="d-flex align-items-center justify-content-center pb-4">
+                          <div className="d-flex align-items-center justify-content-center pb-4">
                             <p className="mb-0 me-2">Do not have an account?</p>
                             <Link href="signup" className="btn btn-outline-danger">Sign up</Link>
                           </div>
 
-                      </form>
+                        </form>
 
-                    </div>
-                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      </div>
+                      <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                      <Image  width={400} height={400} src="/logo.jpg"
-                        className="img-fluid m-5" alt="Sample image" />
+                        <Image width={400} height={400} src="/logo.jpg"
+                          className="img-fluid m-5" alt="Sample image" />
 
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div></>
-)
+        </section>
+      </div></>
+  )
   // return (
   //   <>
   //     <Head>
@@ -232,8 +232,8 @@ return (
 
   //                     </div>
   //                   </div>
-                    
-                
+
+
   //                 </div>
   //               </div>
   //             </div>
