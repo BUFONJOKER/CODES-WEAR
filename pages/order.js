9cdfe45bd801e9cdb33f4b4e589ebbab0fec56d8
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-05 00:25:13 +0500

    order page populated

--- pages/order.js
+++ pages/order.js
@@ -1,49 +1,88 @@
 import React from 'react'
 import Image from 'next/image'
+import Order from "@/models/Order";
 
-export default function Order() {
+export default function MyOrder({ orders}) {
+  console.log(orders.products)
   return (
     <div>
-      <section class="text-gray-600 body-font overflow-hidden text-white mt-5 mb-5">
-        <div class="container px-5 py-24 mx-auto text-white">
-          <div class="row justify-content-center align-items-center ">
-            <div class="col-lg-6 col-md-12 mb-4 mb-lg-0">
-              <h2 class="text-sm  text-white">BRAND NAME</h2>
-              <h1 class="text-white font-weight-bold mt-2 mb-4 text-white">Animated Night Hill Illustrations</h1>
-              <div class="d-flex mb-4">
-                <h3 class="flex-grow-1 text-indigo-500 border-bottom border-indigo-500 py-2 text-lg">Description</h3>
-                <h3 class="flex-grow-1 border-bottom border-gray-300 py-2 text-lg">Quantity</h3>
-                <h3 class="flex-grow-1 border-bottom border-gray-300 py-2 text-lg">Total</h3>
+      <section className="text-gray-600 body-font overflow-hidden text-white mt-5 mb-5">
+        <div className="container  mx-auto text-white">
+          <div className="row justify-content-center align-items-center ">
+            <div className="col-lg-10 col-md-12 mb-4 mb-lg-0">
+              <h2 className="text-sm  text-white">BRAND NAME</h2>
+              <h1 className="text-white font-weight-bold text-white">Order ID: #{orders._id}</h1>
+              <div className="d-flex mb-4">
+                <h3 className="flex-grow-1 text-indigo-500 border-bottom border-indigo-500 py-2 text-lg">Description</h3>
+                <h3 className="flex-grow-1 border-bottom border-gray-300 py-2 text-lg">Quantity</h3>
+                <h3 className="flex-grow-1 border-bottom border-gray-300 py-2 text-lg">Color</h3>
+                <h3 className="flex-grow-1 border-bottom border-gray-300 py-2 text-lg ">Size</h3>
+                <h3 className="flex-grow-1 border-bottom border-gray-300 py-2 text-lg">Price</h3>
               </div>
-              <div className="row">
-                <ul className="col mx-4">
-                  <li>Shirt</li>
+              {
+                Object.keys(orders.products).map((item) => {
+                  return (
+                    <React.Fragment key={orders.products[item]._id}>
+                      <div className="row">
+                <ul className="col mx-5">
+                  <li>{orders.products[item].name}</li>
                 
                 </ul>
+
                 <ul className="col mx-5">
-                  <li>1</li>
+                  <li>{orders.products[item].quantity}</li>
                  
                 </ul>
-                <ul className="col">
-                  <li>1250</li>
+                <ul className="col mx-4">
+                  <li>{orders.products[item].variant}</li>
+                 
+                </ul>
+                <ul className="col mx-5">
+                  <li>{orders.products[item].size}</li>
+                 
+                </ul>
+               
+                <ul className="col mx-3">
+                  <li>Rs. {orders.products[item].price}</li>
                
                 </ul>
-              </div>
+              </div>  
+
+              
+                    </React.Fragment>
+                  )
+                })
+              }
          
-              <div class="d-flex align-items-center">
-                <span class="font-weight-bold h2 text-white "> Total: Rs.1250</span>
-                <button class="btn btn-primary fs-5 fw-bold  text-white mx-5">Track Order</button>
-          
-                
-              </div>
+             
             </div>
-            <div class="col-lg-6 col-md-12">
+            {/* <div className="col-lg-6 col-md-12">
               <Image src="/joker_sticker.jpg" alt="Picture of the author" width={300} height={500} />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
 
     </div>
   )
+}
+
+
+
+export async function getServerSideProps(context) {
+  
+    const orders = await Order.findById(context.query.id);
+    // console.log("avavga" + orders) 
+    // Query your database for orders
+
+    // Convert the orders data to a object
+    const data = JSON.parse(JSON.stringify(orders));
+    // console.log("daat" + data)
+
+    return {
+      props: {
+        orders: data, // Use a more appropriate prop name, e.g., "orders" instead of "order"
+      },
+    };
+  
 }
